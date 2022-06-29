5eea9fb8fdbe3f25628b39f375ee53e93284591d
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-06-29 20:21:47 +1200

    Cleaned up commented code

--- GameboyColourDecolouriser/Program.cs
+++ GameboyColourDecolouriser/Program.cs
@@ -1,33 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GameboyColourDecolouriser;
 using Spectre.Console;
-using Spectre.Console.Cli;
 using System.CommandLine;
-using System.Drawing;
 using Command = System.CommandLine.Command;
-
-// https://docs.microsoft.com/en-us/dotnet/standard/commandline/
-// and spectre.console
-Console.WriteLine("Hello, World!");
-
-var threeColourTile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-    @"GitHub\Pokemon-gen-2-style-tilemap/Original/Grass-flat.png");
-
-threeColourTile = @"C:\Users\Niko Uusitalo\Documents\GitHub\Pokemon-gen-2-style-tilemap\ayylmao.png";
-//threeColourTile = @"C:\Users\Niko Uusitalo\Documents\GitHub\Little-Mokki-In-The-Woods\LittleMokkiInTheWoods\assets\backgrounds\Mokki-area-export.png";
-threeColourTile = @"C:\Users\Niko Uusitalo\Documents\GitHub\GameboyColour-Decolouriser\etc\mokkiarea-truecolour.png";
-
-//var app = new CommandApp();
-//app.Configure(config =>
-//{
-//    config.AddDelegate("foo", Foo)
-//        .WithDescription("Foos the bars");
-
-//    config.AddDelegate<BarSettings>("bar", Bar)
-//        .WithDescription("Bars the foos"); ;
-//});
-
-//return app.Run(args);
 
 // use system.commandline to understand even though i could use spectuire.console.cli
 // move to dependency injection, inject the progrss tasks, and everything else
@@ -91,13 +66,6 @@ decolouriseCommand.SetHandler((input, output) =>
     }
 },
 inputFile, outputFile);
-
-
-AnsiConsole.WriteLine("ayylmao2");
-
-
-
-AnsiConsole.WriteLine("ayylmao2");
 
 rootCommand.Invoke(args);
 
