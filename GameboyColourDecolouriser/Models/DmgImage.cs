37b342f985e6492a430b8ff4ce0aab2e864bd550
Niko <983351+nikouu@users.noreply.github.com>
2022-07-07 16:29:57 +1200

    First piece of work to decouple System.Drawing from the processing code

---
+++ GameboyColourDecolouriser/Models/DmgImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameboyColourDecolouriser.Models
+{
+    public abstract class DmgImage
+    {
+        private readonly int _width;
+        private readonly int _height;
+        private readonly ITile[,] _tiles;
+
+        public ITile[,] Tiles => _tiles;
+        public int Width => _width;
+        public int Height => _height;
+
+        public DmgImage(int width, int height, ITile[,] tiles)
+        {
+            _width = width;
+            _height = height;
+            _tiles = tiles;
+        }
+    }
+}
---
+++ GameboyColourDecolouriser/Models/GbImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameboyColourDecolouriser.Models
+{
+    public class GbImage : DmgImage
+    {
+        public GbImage(int width, int height, ITile[,] tiles) : base(width, height, tiles)
+        {
+        }
+    }
+}
